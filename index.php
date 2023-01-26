2f840ec9d044dc6eef781eeec1dc0d86a202860b
Vittor Rescarolli de Medeiros <vittorrmedeiros@gmail.com>
2023-01-26 10:29:33 -0300

    php-if: arrays

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$arr = [
+    'nome' => 'José',
+    'idade' => 18,
+    'altura' => 1.60,
+    'trabalha' => true,
+    'profissão' => 'programador',
+    'cônjuge' => true,
+    'mora_soz' => false,
+];
+
+$nome = $arr['nome'];
+$idade = $arr['idade'];
+$altura = $arr['altura'];
+$trabalha = $arr['trabalha'];
+$profissao = $arr['profissão'];
+$conjuge = $arr['cônjuge']; 
+$mora_soz = $arr['mora_soz'];
+
+?>
