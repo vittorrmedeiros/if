c6b5123a438bc8b70e331401a9214e8497fd6f13
Vittor Rescarolli de Medeiros <vittorrmedeiros@gmail.com>
2023-01-26 10:35:07 -0300

    php-if: if

--- index.php
+++ index.php
@@ -18,4 +18,36 @@ $profissao = $arr['profissão'];
 $conjuge = $arr['cônjuge']; 
 $mora_soz = $arr['mora_soz'];
 
+echo "Seu nome é $nome <br>";
+
+if($idade >= 18) {
+    echo "$nome é maior de idade, tem $idade anos. <br>";
+} else {
+    echo "$nome é menor de idade. <br>";
+}
+
+if($altura > 1.60) {
+    echo "$nome possuí uma alta estatura, tem $altura, acima do padrão de alta estatura. <br>";
+} else {
+    echo "$nome possuí uma baixa estatura, tem $altura, abaixo do padrão de alta estatura. <br>";
+}
+
+if($trabalha == true) {
+    echo "$nome trabalha, ele é $profissao. <br>";
+} else {
+    echo "$nome é desempregado. <br>";
+}
+
+if($conjuge == true) {
+echo "$nome é casado. <br>";
+} else {
+echo "$nome é solteiro. <br>";
+}
+
+if($mora_soz == true) {
+echo "$nome mora sozinho. <br>";
+} else {
+echo "$nome não mora sozinho. <br>";
+}
+
 ?>
